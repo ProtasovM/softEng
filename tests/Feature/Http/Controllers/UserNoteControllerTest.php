de605b7471ad38293552c7abd7a4feeeed0a6565
mprotasov <protasovmikhailevgenivich@gmail.com>
2023-11-13 15:08:30 +0300

    Добавлен тест

--- tests/Feature/Http/Controllers/UserNoteControllerTest.php
+++ tests/Feature/Http/Controllers/UserNoteControllerTest.php
@@ -2,19 +2,69 @@
 
 namespace Tests\Feature\Http\Controllers;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\User;
+use App\Models\UserNote;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use Laravel\Passport\ClientRepository;
 use Tests\TestCase;
 
 class UserNoteControllerTest extends TestCase
 {
+    public User $user;
+    public string $token;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create();
+
+        $clientRepository = new ClientRepository();
+        $client = $clientRepository->createPersonalAccessClient(
+            $this->user->id,
+            'user',
+            '/',
+        );
+
+        DB::table('oauth_personal_access_clients')->insert([
+            'client_id' => $client->id,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+
+        $this->token = $this->user->createToken('test')->accessToken;
+
+        $this->assertIsString($this->token);
+    }
+
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
+    public function test403(): void
     {
-        $response = $this->get('/');
+        $response = $this->getJson('/api/user-notes/');
 
-        $response->assertStatus(200);
+        $response->assertStatus(403);
+    }
+
+    public function testList(): void
+    {
+        $response = $this->withToken($this->token)
+            ->getJson('/api/user-notes/');
+
+        $response->assertStatus(204);
+    }
+
+    public function testCreate(): void
+    {
+        $response = $this->withToken($this->token)
+            ->postJson(
+                '/api/user-notes/',
+                UserNote::factory()->create()->toArray()
+            );
+
+        $response->assertStatus(201);
     }
 }
